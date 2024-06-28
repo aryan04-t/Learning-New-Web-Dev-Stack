e4229ca76703702721734fb74a44acf127f497c3
Aryan Tomar <aryan@myillumine.com>
2024-06-28 12:57:02 +0530

    Added "Edit" and "Delete" Icons Using react-icons and Switched Row Display to Grid

--- frontend/src/components/DisplayFruits.jsx
+++ frontend/src/components/DisplayFruits.jsx
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from 'react'
 import { app } from '../firebase/firebase.config';
 import { getDatabase, ref, get } from 'firebase/database';
 
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+
 
 const DisplayFruits = () => {
 
@@ -26,22 +29,34 @@ const DisplayFruits = () => {
     getFruitsArray(); 
   }, []) 
 
-  const rowColsCSS = 'w-[50%] h-full text-center flex items-center justify-center'; 
+  const tableHeaderRowColCSS = 'text-center flex items-center justify-center border-l-[2px] border-black'; 
+  const rowColsCSS = 'text-center flex items-center justify-center'; 
+
+  const iconsCSS = 'hover:cursor-pointer text-xl h-[24px] w-[24px] rounded-full flex justify-center items-center '; 
 
   return (
-    <div className='w-[400px] h-[480px] rounded-lg overflow-hidden border-[1px] border-white text-white'>
+    <div className='w-[450px] h-[480px] rounded-lg overflow-hidden border-[1px] border-white text-white'>
       <table className='h-full w-full flex flex-col'> 
-        <tr className='bg-white text-black h-[80px] w-full flex border-b-[1px] border-white'> 
-          <th className={rowColsCSS}> Fruit Name </th>
-          <th className={rowColsCSS}> Fruit Colour </th>
+        <tr className='grid-flexible-cols bg-white text-black h-[80px] w-full flex'> 
+          <th className='text-center flex items-center justify-center'> Fruit Name </th>
+          <th className={tableHeaderRowColCSS}> Fruit Colour </th>
+          <th className={tableHeaderRowColCSS}> Options </th>
         </tr>
         <div className='h-[400px] w-full overflow-y-auto scrollbar'> 
           {
             fruitsArray?.map( (fruitObj, index) => {
               return(
-                <tr key={fruitObj?._id} className={`flex h-[80px] w-full border-t-[1px] ${ (index > 0 ? 'border-white' : 'border-transparent')}`} >
+                <tr key={fruitObj?._id} className={`grid-flexible-cols h-[80px] w-full border-t-[1px] ${ (index > 0 ? 'border-white' : 'border-transparent')}`} >
                   <td className={rowColsCSS}> {fruitObj?.fruitName} </td>
                   <td className={rowColsCSS}> {fruitObj?.fruitColour} </td>
+                  <td className='text-center flex items-center justify-center gap-4'> 
+                    <span className={iconsCSS + 'hover:text-sky-500'}>
+                      <MdEdit />
+                    </span>
+                    <span className={iconsCSS + 'hover:text-red-500'}>
+                      <MdDelete />
+                    </span>
+                  </td>
                 </tr>
               )  
             })
